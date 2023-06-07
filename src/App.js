e25dba35bd1675134d170db2f67212d0487e5663
Ananya Seth <sethananya58@gmail.com>
2023-06-08 01:01:25 +0530

    Login screen created and added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {
   Routes,
   Route
 } from "react-router-dom";
+import Login from './screens/Login';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <div>
         <Routes>
           <Route exact path="/" element={<Home/>}/>
+          <Route exact path="/login" element={<Login/>}/>
         </Routes>
       </div>
     </Router>
