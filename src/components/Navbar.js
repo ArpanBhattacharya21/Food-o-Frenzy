6e1f7d468b0a8028bf4125fc96ca70ce6fc4552c
Arpan Bhattacharya <arpan.bhatta.21@gmail.com>
2023-06-10 22:54:17 +0530

    FE/BE: SignUp functionality with validation and DB

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,26 +10,25 @@ export default function Navbar() {
             Food-o-Frenzy
           </Link>
           <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
+            class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation"
           >
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
               <li class="nav-item">
-                <Link class="nav-link active" aria-current="page" to="#">
+                <Link class="nav-link" aria-current="page" to="#">
                   Home
                 </Link>
               </li>
               <li class="nav-item">
                 <Link class="nav-link" to="/login">
                   Login
+                </Link>
+              </li>
+              <li class="nav-item">
+                <Link class="nav-link" to="/createuser">
+                  SignUp
                 </Link>
               </li>
             </ul>
