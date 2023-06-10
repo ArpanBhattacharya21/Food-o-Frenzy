f8caf48395a447cf3de905c6c48b93098a77f650
Arpan Bhattacharya <arpan.bhatta.21@gmail.com>
2023-06-10 22:53:27 +0530

    BE: Access Control for API connection & signup pg

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const app = express()
 const port = 5000
-
 const mongoDB = require("./db")
 mongoDB();
+
+app.use((req,res,next)=>{
+  res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+})
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
