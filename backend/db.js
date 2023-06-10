0f069e8f56eba43a084e82a17bb8fbd1a6a2da64
Arpan Bhattacharya <arpan.bhatta.21@gmail.com>
2023-06-10 21:21:18 +0530

    BE/DBA: Conneciton URI changed

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
-const mongoURI = 'mongodb+srv://foodofrenzy:aran1521@cluster0.qeidlvm.mongodb.net/foodofrenzy?retryWrites=true&w=majority';
-//const mongoURI2 = 'mongodb+srv://foodofrenzy:aran1521@cluster0.qeidlvm.mongodb.net/?retryWrites=true&w=majority'; 
-//const mongoURI3 = 'mongodb://foodofrenzy:aran1521@ac-lmffe97-shard-00-00.qeidlvm.mongodb.net:27017,ac-lmffe97-shard-00-01.qeidlvm.mongodb.net:27017,ac-lmffe97-shard-00-02.qeidlvm.mongodb.net:27017/?ssl=true&replicaSet=atlas-a4t3g9-shard-0&authSource=admin&retryWrites=true&w=majority';
+//const mongoURI = 'mongodb+srv://foodofrenzy:aran1521@cluster0.qeidlvm.mongodb.net/foodofrenzy?retryWrites=true&w=majority';
+const mongoURI = 'mongodb://foodofrenzy:aran1521@ac-lmffe97-shard-00-00.qeidlvm.mongodb.net:27017,ac-lmffe97-shard-00-01.qeidlvm.mongodb.net:27017,ac-lmffe97-shard-00-02.qeidlvm.mongodb.net:27017/foodofrenzy?ssl=true&replicaSet=atlas-a4t3g9-shard-0&authSource=admin&retryWrites=true&w=majority';
 
 const mongoDB = async()=>{
     await mongoose.connect(mongoURI,{ useNewUrlParser: true}, async(err,result)=>{
