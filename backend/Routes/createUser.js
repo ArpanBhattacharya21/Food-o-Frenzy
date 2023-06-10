96bc6ed48a80325fd57941297ef97f7f04a57f55
Arpan Bhattacharya <arpan.bhatta.21@gmail.com>
2023-06-10 21:21:36 +0530

    BE: Express validations added in createUser.js

--- backend/Routes/createUser.js
+++ backend/Routes/createUser.js
@@ -1,20 +1,32 @@
-const express = require('express')
-const router = express.Router()
-const User=require('../models/User')
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User");
+const { body, validationResult } = require("express-validator");
 
-router.post("/createuser",async (req,res)=>{
-    try{
-        await User.create({
-            name: req.body.name,
-            password: req.body.password,
-            email: req.body.email,
-            location: req.body.location
-        })
-        res.json({success:true});
-    } catch (error){
-        console.log(error)
-        res.json({success:false});
+router.post(
+  "/createuser",
+  body("email",'Invalid Email').isEmail(),
+  body("name").isLength({ min: 3 }),
+  body("password",'Incorrect Password').isLength({ min: 5 }),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-})
+
+    try {
+      await User.create({
+        name: req.body.name,
+        password: req.body.password,
+        email: req.body.email,
+        location: req.body.location,
+      });
+      res.json({ success: true });
+    } catch (error) {
+      console.log(error);
+      res.json({ success: false });
+    }
+  }
+);
 
 module.exports = router;
