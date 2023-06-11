e1d6d04bccf7f06ce38b0fb7ac6e0c7b5bcd27e7
Arpan Bhattacharya <arpan.bhatta.21@gmail.com>
2023-06-11 16:24:30 +0530

    Inte: Navigate to home

--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Signup() {
     const [credentials, setcredentials] = useState({name:"",email:"",password:"",geolocation:""})
+    let navigate = useNavigate()
 
     const handleSubmit = async(e)=>{
         e.preventDefault();     //synthetic event
@@ -20,6 +21,13 @@ export default function Signup() {
         if(!json.success){
             alert("Enter valid credentials!");
         }
+
+        if(json.success){
+            alert(credentials.name + " is registered!!");
+            navigate("/");
+        }
+
+
     }
 
 
