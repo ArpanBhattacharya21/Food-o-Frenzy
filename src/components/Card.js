75b34978fc68206cee1a0213c55d78e99d9e2cb5
Ananya Seth <sethananya58@gmail.com>
2023-06-13 00:25:57 +0530

    Logout functionality and navbar modifications

--- src/components/Card.js
+++ src/components/Card.js
@@ -11,7 +11,7 @@ export default function Card(props) {
           className="card mt-3"
           style={{ width: "18rem", maxHeight: "" }}
         >
-          <img src={props.imgSrc} className="card-img-top" alt="..." />
+          <img src={props.imgSrc} className="card-img-top" alt="..." style={{height:"180px", objectFit:"contain"}}/>
           <div className="card-body">
             <h5 className="card-title">{props.foodName}</h5>
             <p className="card-text">{props.desc}</p>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,14 @@
 import React from "react";
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
 
 export default function Navbar() {
+
+  const navigate = useNavigate();
+  const handleLogout = ()=>{
+    localStorage.removeItem("authToken");
+    navigate("/login");
+  }
+
   return (
     <div>
       <nav class="navbar navbar-expand-lg navbar-dark bg-success">
@@ -15,23 +22,27 @@ export default function Navbar() {
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
+            <ul class="navbar-nav me-auto mb-2">
               <li class="nav-item">
-                <Link class="nav-link" aria-current="page" to="#">
-                  Home
-                </Link>
+                <Link class="nav-link active fs-5" aria-current="page" to="/">Home</Link>
               </li>
-              <li class="nav-item">
-                <Link class="nav-link" to="/login">
-                  Login
-                </Link>
-              </li>
-              <li class="nav-item">
-                <Link class="nav-link" to="/createuser">
-                  SignUp
-                </Link>
-              </li>
+              {(localStorage.getItem("authToken"))?
+                <li class="nav-item">
+                  <Link class="nav-link active fs-5" aria-current="page" to="/">My Orders</Link>
+                </li>
+              : ""}
             </ul>
+            {(!localStorage.getItem("authToken"))?
+            <div className="d-flex">
+                <Link className="btn bg-white text-success mx-1" to="/login">Login</Link>
+                <Link className="btn bg-white text-success mx-1" to="/createuser">SignUp</Link>
+            </div>
+            :
+            <div>
+              <div className="btn bg-white text-success mx-2">My Cart</div>
+              <div className="btn bg-white text-danger mx-2" onClick={handleLogout}>Logout</div>
+            </div>
+            }
           </div>
         </div>
       </nav>
