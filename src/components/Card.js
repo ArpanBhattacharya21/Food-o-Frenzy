4d1729e1b3ffcd6b347219534ff2c0ba883cd00e
Arpan Bhattacharya <arpan.bhatta.21@gmail.com>
2023-06-16 00:33:08 +0530

    FE/BE: Add to cart functionality Fully ADDED!

--- src/components/Card.js
+++ src/components/Card.js
@@ -9,11 +9,38 @@ export default function Card(props) {
   let priceOptions= Object.keys(options);
   const [qty, setQty] = useState(1)
   const [size, setSize] = useState("")
-  const handleAddToCart = async ()=>{
-      await dispatch({type:"ADD",id:props.foodItem._id, name: props.foodItem.name,price: finalPrice, qty: qty, size: size })
-      console.log(data);
+
+  const handleAddToCart = async () => {
+    let food = []
+    for (const item of data) {
+      if (item.id === props.foodItem._id) {
+        food = item;
+
+        break;
+      }
+    }
+    console.log(food)
+    console.log(new Date())
+    if (food !== []) {
+      if (food.size === size) {
+        await dispatch({ type: "UPDATE", id: props.foodItem._id, price: finalPrice, qty: qty })
+        return
+      }
+      else if (food.size !== size) {
+        await dispatch({ type: "ADD", id: props.foodItem._id, name: props.foodItem.name, price: finalPrice, qty: qty, size: size, })
+        console.log("Size different so simply ADD one more to the list")
+        return
+      }
+      return
+    }
+    await dispatch({ type: "ADD", id: props.foodItem._id, name: props.foodItem.name, price: finalPrice, qty: qty, size: size })
+    // setBtnEnable(true)
   }
+
+
+
   let finalPrice = qty * parseInt(options[size]);
+  
   useEffect(()=>{
     setSize(priceRef.current.value)
   },[])
--- src/screens/Cart.js
+++ src/screens/Cart.js
@@ -1,16 +1,41 @@
 import React from "react";
-import { useCart, useDispatchCart } from "../components/contextReducer";
+import Delete from '@material-ui/icons/Delete';
+import { useDispatchCart, useCart } from "../components/contextReducer";
 
 export default function Cart() {
   let data = useCart();
   let dispatch = useDispatchCart();
-  if (data.length === 0) {
+
+  if (data.length === 0 ) {
     return (
       <div>
         <div className="m-5 w-100 text-center fs-3">The Cart is Empty</div>
       </div>
     );
   }
+
+  const handleCheckOut = async () => {
+    let userEmail = localStorage.getItem("userEmail");
+    // console.log(data,localStorage.getItem("userEmail"),new Date())
+    let response = await fetch("http://localhost:5000/api/auth/orderData", {
+      // credentials: 'include',
+      // Origin:"http://localhost:3000/login",
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        order_data: data,
+        email: userEmail,
+        order_date: new Date().toDateString()
+      })
+    });
+    console.log("JSON RESPONSE:::::", response.status)
+    if (response.status === 200) {
+      dispatch({ type: "DROP" })
+    }
+  }
+
   let totalPrice = data.reduce((total, food) => total + food.price, 0);
   return (
     <div>
