80f916a508da4e4977a917a2278bbfe83aff7103
Arpan Bhattacharya <arpan.bhatta.21@gmail.com>
2023-06-16 00:32:08 +0530

    FE: Dynamic Badge

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,9 +3,12 @@ import { Link, useNavigate } from "react-router-dom";
 import { Badge } from "react-bootstrap";
 import Cart from '../screens/Cart';
 import Modal from '../Modal';
+import { useCart } from "./contextReducer";
+
 
 export default function Navbar() {
   const [cartView,setCartView] = useState(false)
+  let data = useCart();
   const navigate = useNavigate();
   const handleLogout = () => {
     localStorage.removeItem("authToken");
@@ -63,7 +66,7 @@ export default function Navbar() {
               <div>
                 <div className="btn bg-white text-success mx-2" onClick={()=> {setCartView(true)}}>
                   My Cart{" "}
-                  <Badge pill bg="danger"> 2 </Badge>
+                  <Badge pill bg="danger"> {data.length} </Badge>
                 </div>
 
                 {cartView ? <Modal onClose={() => setCartView(false)}><Cart></Cart></Modal> : ""}
