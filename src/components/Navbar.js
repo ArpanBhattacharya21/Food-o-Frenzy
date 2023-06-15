0a53999cc7ae39f189de4f1e86eb04a025c75ee0
Arpan Bhattacharya <arpan.bhatta.21@gmail.com>
2023-06-15 18:39:51 +0530

    FE: Cart page and Modal Created!

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,16 @@
-import React from "react";
-import {Link, useNavigate} from 'react-router-dom'
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { Badge } from "react-bootstrap";
+import Cart from '../screens/Cart';
+import Modal from '../Modal';
 
 export default function Navbar() {
-
+  const [cartView,setCartView] = useState(false)
   const navigate = useNavigate();
-  const handleLogout = ()=>{
+  const handleLogout = () => {
     localStorage.removeItem("authToken");
     navigate("/");
-  }
+  };
 
   return (
     <div>
@@ -18,32 +20,62 @@ export default function Navbar() {
             Food-o-Frenzy
           </Link>
           <button
-            class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav me-auto mb-2">
               <li class="nav-item">
-                <Link class="nav-link active fs-5" aria-current="page" to="/">Home</Link>
+                <Link class="nav-link active fs-5" aria-current="page" to="/">
+                  Home
+                </Link>
               </li>
-              {(localStorage.getItem("authToken"))?
+              {localStorage.getItem("authToken") ? (
                 <li class="nav-item">
-                  <Link class="nav-link active fs-5" aria-current="page" to="/">My Orders</Link>
+                  <Link class="nav-link active fs-5" aria-current="page" to="/">
+                    My Orders
+                  </Link>
                 </li>
-              : ""}
+              ) : (
+                ""
+              )}
             </ul>
-            {(!localStorage.getItem("authToken")) ?
-            <div className="d-flex">
-                <Link className="btn bg-white text-success mx-1" to="/login">Login</Link>
-                <Link className="btn bg-white text-success mx-1" to="/createuser">SignUp</Link>
-            </div>
-            :
-            <div>
-              <div className="btn bg-white text-success mx-2">My Cart {" "}
-              <Badge pill bg="danger">2</Badge></div>
-              <div className="btn bg-white text-danger mx-2" onClick={handleLogout}>Logout</div>
-            </div>
-            }
+            {!localStorage.getItem("authToken") ? (
+              <div className="d-flex">
+                <Link className="btn bg-white text-success mx-1" to="/login">
+                  Login
+                </Link>
+                <Link
+                  className="btn bg-white text-success mx-1"
+                  to="/createuser"
+                >
+                  SignUp
+                </Link>
+              </div>
+            ) : (
+              <div>
+                <div className="btn bg-white text-success mx-2" onClick={()=> {setCartView(true)}}>
+                  My Cart{" "}
+                  <Badge pill bg="danger"> 2 </Badge>
+                </div>
+
+                {cartView ? <Modal onClose={() => setCartView(false)}><Cart></Cart></Modal> : ""}
+                
+                <div
+                  className="btn bg-white text-danger mx-2"
+                  onClick={handleLogout}
+                >
+                  Logout
+                </div>
+              </div>
+            )}
           </div>
         </div>
       </nav>
